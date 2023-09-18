45e55c89c8c292f69a1143cb9405aac0c7163575
hyunwoomemo <archihw94@gmail.com>
2023-09-18 22:33:18 +0900

    library: victory 이용해서 차트 그리기

--- screens/Detail.jsx
+++ screens/Detail.jsx
@@ -1,27 +1,54 @@
-import React, { useEffect } from 'react'
-import styled from 'styled-components/native'
-import { Icons } from '../components/Coin'
-import {useQuery} from 'react-query'
-import { history, info } from '../api'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components/native";
+import { Icons } from "../components/Coin";
+import { useQuery } from "react-query";
+import { history, info } from "../api";
+import { BLACK_COLOR } from "../colors";
+import { VictoryChart, VictoryLine , VictoryScatter} from "victory-native";
 
-const Container = styled.View``
+const Container = styled.ScrollView`
+  flex: 1;
+  background-color: ${BLACK_COLOR};
+`;
 
-const Detail = ({ navigation, route: {params: {symbol, id}}}) => {
-  console.log(id)
+const Detail = ({
+  navigation,
+  route: {
+    params: { symbol, id },
+  },
+}) => {
   useEffect(() => {
     navigation.setOptions({
-      headerTitle: () => <Icons source={{ uri: `https://coinicons-api.vercel.app/api/icon/${symbol.toLowerCase()}` }}></Icons>
-    })
-  }, [])
-  
-  const {isLoading: infoLoading, data: infoData} = useQuery(['coinInfo', id], info)
-  const {isLoading: historyLoading, data: historyData} = useQuery(['coinHistory', id], history)
+      headerTitle: () => <Icons source={{ uri: `https://coinicons-api.vercel.app/api/icon/${symbol.toLowerCase()}` }}></Icons>,
+    });
+  }, []);
 
-  console.log(infoData)
+  const { isLoading: infoLoading, data: infoData } = useQuery(["coinInfo", id], info);
+  const { isLoading: historyLoading, data: historyData } = useQuery(["coinHistory", id], history);
+
+  const [victoryData, setVictoryData] = useState(null);
+
+  useEffect(() => {
+    if (historyData) {
+      setVictoryData(historyData.map((price) => ({ x: new Date(price.timestamp).getTime(), y: price.price })));
+    }
+  }, [historyData]);
 
   return (
-    <Container></Container>
-  )
-}
+    <Container>
+      {!victoryData ? null : (
+        <VictoryChart height={360}>
+          <VictoryLine
+            animate
+            interpolation="monotoneX"
+            data={victoryData} style={{ data: { stroke: '#1abc9c' } }} />
+          <VictoryScatter
+            data={victoryData} style={{data: {fill: '#1abc9c'}}}
+          />
+        </VictoryChart>
+      )}
+    </Container>
+  );
+};
 
-export default Detail
+export default Detail;
